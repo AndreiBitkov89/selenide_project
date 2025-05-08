0c952f7845a43061e6a852e06d36f1c078ae9ba3
AndreiBitkov89 <AndreiBitkov89@users.noreply.github.com>
2025-05-08 08:46:22 +0000

    deploy: 6be4d6279fb3a2d9bd9227bfdc11a870886e9bcb

--- src/test/java/config/ConfigProvider.java
+++
@@ -1,7 +0,0 @@
-package config;
-
-import org.aeonbits.owner.ConfigFactory;
-
-public class ConfigProvider {
-    public static final AppConfig CONFIG = ConfigFactory.create(AppConfig.class, System.getProperties());
-}
--- src/test/java/pages/registrationpage/RegistrationPageInterface.java
+++
@@ -1,9 +0,0 @@
-package pages.registrationpage;
-
-import helpers.AlertTypes;
-import valueObjects.User;
-
-public interface RegistrationPageInterface {
-
-    public RegistrationPageInterface registration(User user, AlertTypes alert);
-}
